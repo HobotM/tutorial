34c6527fd17d83bca82ee851b70b2fe5357150cc
HobotM <mateuszhobot@gmail.com>
2022-10-17 19:27:35 +0100

    Tutorial Vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,39 @@
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <section>
+    <p v-if="likes > 0">
+      this presentation has {{ likes }} likes
+    </p>
+
+    <p v-else>
+      nobody likes it
+
+    </p>
+    <button v-on:click="presentation()">
+      Like
+    </button>
+  </section>
+
 </template>
+
+<script>
+import Vue from 'vue'
+export default {
+
+  data () {
+    return {
+      likes: 0
+    }
+  },
+  methods: {
+    presentation () {
+      this.likes++
+    }
+  }
+
+}
+
+</script>
 
 <style lang="scss">
 #app {
@@ -25,6 +54,34 @@ nav {
     &.router-link-exact-active {
       color: #42b983;
     }
+  }
+}
+
+button {
+  display: inline-block;
+  padding: 0.35em 1.2em;
+  border: 0.1em solid #FFFFFF;
+  margin: 0 0.3em 0.3em 0;
+  border-radius: 0.12em;
+  box-sizing: border-box;
+  text-decoration: none;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 300;
+  color: white;
+  text-align: center;
+  transition: all 0.2s;
+  background-color: blue;
+}
+
+button:hover {
+  color: white;
+  background-color: black;
+}
+
+@media all and (max-width:30em) {
+  button {
+    display: block;
+    margin: 0.4em auto;
   }
 }
 </style>
